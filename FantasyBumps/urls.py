2d65dd3be77a795aa7a1364b30562ae83a798133
Joe Hitchen <hitchenjoe@gmail.com>
2019-03-15 17:52:38 +0000

    Added gender constants

--- FantasyBumps/urls.py
+++ FantasyBumps/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from django.views.generic.base import TemplateView
 
 from . import views
+from external.constants import genders
 
 
 app_name = 'fantasybumps'
 urlpatterns = [
-    path('men/', views.MarketView.as_view(), {'gender': 'M'}, name = 'men'),
-    path('women/', views.MarketView.as_view(), {'gender': 'W'}, name = 'women'),
+    path('men/', views.MarketView.as_view(), {'gender': genders.MENS}, name = 'men'),
+    path('women/', views.MarketView.as_view(), {'gender': genders.WOMENS}, name = 'women'),
     path('', views.IndexView.as_view(), name = 'index'),
 ]
---
+++ external/constants.py
@@ -0,0 +1,5 @@
+
+class genders:
+    MENS = 'M'
+    WOMENS = 'W'
+
