75b8f137a37ddba651e586f839ff79e94b7ceefc
Joe Hitchen <hitchenjoe@gmail.com>
2019-03-31 17:16:00 +0100

    Added form for selling athletes

--- FantasyBumps/forms.py
+++ FantasyBumps/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
+from external import models as ext_models
+from external.constants import genders
+
 from . import models
 
 
@@ -17,4 +20,33 @@ class Buy(forms.ModelForm):
         rower.team = team
         rower.save()
         return rower
+
+
+
+class Sell(forms.Form):
+    
+    seat = forms.ModelChoiceField(queryset = ext_models.Seat.objects.all())
+    gender = forms.ChoiceField(
+        choices = [
+            (genders.MENS, "Men's"),
+            (genders.WOMENS, "Women's"),
+        ],
+    )
+    
+    def __init__(self, *args, team, **kwargs):
+        """Stores the 'team' keyword argument."""
+        
+        super().__init__(*args, **kwargs)
+        self.team = team
+    
+    
+    def save(self):
+        """Deletes all rower instances matching the team, seat, and gender. Returns the gender."""
+        
+        models.Rower.objects.filter(
+            team = self.team,
+            seat = self.cleaned_data['seat'],
+            crew__gender = self.cleaned_data['gender'],
+        ).delete()
+        return self.cleaned_data['gender']
 
