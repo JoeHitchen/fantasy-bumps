49d6a6b359ee5786bb72915bb7ca4457f007942d
Joe Hitchen <hitchenjoe@gmail.com>
2019-05-03 20:18:20 +0100

    Moved to multiline strings for templates

--- FantasyBumps/templatetags/fantasy_tags.py
+++ FantasyBumps/templatetags/fantasy_tags.py
@@ -58,31 +58,31 @@ def bungline_avatar(bungline):
     return format_html('<span class="bungline-avatar">{}</span>', text)
 
 
-@register.inclusion_tag(template.Template(
-    '<tr>\n'
-    '  <td>{{ bungline }}</td>\n'
-    '  <td>{{ tag_crew }}</td>\n'
-    '  <td>£100</td>\n'
-    '  <td><a href="{% url \'fantasybumps:buy\' %}" class="btn btn-sm btn-primary">Buy</a></td>\n'
-    '</tr>\n',
-))
+@register.inclusion_tag(template.Template('''
+  <tr>
+    <td>{{ bungline }}</td>
+    <td>{{ tag_crew }}</td>
+    <td>£100</td>
+    <td><a href="{% url \'fantasybumps:buy\' %}" class="btn btn-sm btn-primary">Buy</a></td>
+  </tr>
+'''))
 def market_division_row(bungline, crew):
     return {'bungline': bungline, 'tag_crew': crew}
 
 
-@register.inclusion_tag(template.Template(
-    '{% load fantasy_tags %}\n'
-    '<table class="table table-sm table-bordered table-hover">'
-    '  <thead class="thead-dark">'
-    '    <tr><th colspan="4">{{ gender }}\'s Division {{ number }}</th></tr>'
-    '  </thead>'
-    '  <tbody>'
-    '  {% for position in division %}'
-    '    {% market_division_row forloop.counter position.crew %}'
-    '  {% endfor %}'
-    '  </tbody>'
-    '</table>',
-))
+@register.inclusion_tag(template.Template('''
+  {% load fantasy_tags %}
+  <table class="table table-sm table-bordered table-hover">
+    <thead class="thead-dark">
+      <tr><th colspan="4">{{ gender }}\'s Division {{ number }}</th></tr>
+    </thead>
+    <tbody>
+    {% for position in division %}
+      {% market_division_row forloop.counter position.crew %}
+    {% endfor %}
+    </tbody>
+  </table>
+'''))
 def market_division_box(division, gender, number):
     return {'division': division, 'gender': gender, 'number': number}
 
@@ -93,46 +93,46 @@ def seat_avatar(seat):
     return format_html('<span class="seat-avatar">{}</span>', text)
 
 
-@register.inclusion_tag(template.Template(
-    '{% load fantasy_tags %}\n'
-    '<tr class="table-{% if rower %}primary{% else %}danger{% endif %}">\n'
-    '  <td>{{ seat|seat_avatar }}</td>\n'
-    '  <td>{% if rower %}{{ rower.crew }}{% else %}Empty{% endif %}</td>\n'
-    '  <td>\n'
-    '    <a href="{% url \'fantasybumps:sell\' %}" class="btn btn-sm btn-primary">Sell</a>\n'
-    '  </td>\n'
-    '</tr>\n',
-))
+@register.inclusion_tag(template.Template('''
+  {% load fantasy_tags %}
+  <tr class="table-{% if rower %}primary{% else %}danger{% endif %}">
+    <td>{{ seat|seat_avatar }}</td>
+    <td>{% if rower %}{{ rower.crew }}{% else %}Empty{% endif %}</td>
+    <td>
+      <a href="{% url \'fantasybumps:sell\' %}" class="btn btn-sm btn-primary">Sell</a>
+    </td>
+  </tr>
+'''))
 def crew_list_row(seat, rower):
     return {'seat': seat, 'rower': rower}
 
 
 @register.inclusion_tag(
-    template.Template(
-        '{% load fantasy_tags %}\n'
-        '<table class="table table-sm table-bordered table-hover">\n'
-        '  <thead class="thead-dark">\n'
-        '    <tr><th colspan="3">Your crew</th></tr>\n'
-        '  </thead>\n'
-        '  <tbody>\n'
-        '  {% for seat, rower in crew %}'
-        '    {% crew_list_row seat rower %}'
-        '  {% endfor %}\n'
-        '  </tbody>\n'
-        '  <tfoot>\n'
-        '    <tr class="table-{% if crew_valid %}success{% else %}danger{% endif %}">\n'
-        '      <th colspan="3">\n'
-        '        This crew is {% if not crew_valid %}not {% endif %}ready to race.\n'
-        '      </th>\n'
-        '    </tr>\n'
-        '    <tr class="table-{% if other_crew_valid %}success{% else %}danger{% endif %}">'
-        '      <th colspan="3">\n'
-        '        Your other crew is {% if not other_crew_valid %}not {% endif %}ready to race.'
-        '      </th>\n'
-        '    </tr>\n'
-        '  </tfoot>\n'
-        '</table>\n',
-    ),
+    template.Template('''
+      {% load fantasy_tags %}
+      <table class="table table-sm table-bordered table-hover">
+        <thead class="thead-dark">
+          <tr><th colspan="3">Your crew</th></tr>
+        </thead>
+        <tbody>
+        {% for seat, rower in crew %}
+          {% crew_list_row seat rower %}
+        {% endfor %}
+        </tbody>
+        <tfoot>
+          <tr class="table-{% if crew_valid %}success{% else %}danger{% endif %}">
+            <th colspan="3">
+              This crew is {% if not crew_valid %}not {% endif %}ready to race.
+            </th>
+          </tr>
+          <tr class="table-{% if other_crew_valid %}success{% else %}danger{% endif %}">
+            <th colspan="3">
+              Your other crew is {% if not other_crew_valid %}not {% endif %}ready to race.
+            </th>
+          </tr>
+        </tfoot>
+      </table>
+    '''),
     takes_context = True,
 )
 def crew_list_box(context):
