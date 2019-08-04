4de41ebe502d1a5a9c38f70d0a9e619930604a9d
Joe Hitchen <hitchenjoe@gmail.com>
2019-08-04 22:56:58 +0100

    Updated query caching notes for sell view

--- FantasyBumps/views_tests.py
+++ FantasyBumps/views_tests.py
@@ -809,11 +809,11 @@ class Test__Sell(TestCase, MessagesMixin):
     def test__query_count(self, market_closes_mock, markets_mock):
         """ Expect:
             (2) Django internals
-            (1) SELECT user's team
+            (1) SELECT user's team  (Could be avoided by comparing on User, but that feels wrong)
             (1) SELECT purchase, crew, team, user, day, event
-            (1) SELECT crew's position that day
+            (1) SELECT crew's position that day  (Affected by caching)
             (2) Transaction overhead
-            (1) Sell action queries
+            (2) Sell action queries
         """
         
         models.Crew.value.cache_clear()
