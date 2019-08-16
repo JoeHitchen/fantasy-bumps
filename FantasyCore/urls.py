2e74c8b15d132a54e9ffe870a28f79b7fa6bd4bd
Joe Hitchen <hitchenjoe@gmail.com>
2019-08-16 20:34:25 +0100

    Added signup functionality

--- FantasyCore/urls.py
+++ FantasyCore/urls.py
@@ -1,6 +1,17 @@
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
+    path(
+        'accounts/signup/',
+        CreateView.as_view(
+            form_class = UserCreationForm,
+            template_name = 'registration/signup.html',
+            success_url = reverse_lazy('login'),
+        ),
+        name = 'signup',
+    ),
     path('', include('FantasyBumps.urls')),
 ]
