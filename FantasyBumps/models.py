JoeHitchen/fantasy-bumps

commit 56a239558e7c9e3570cc028f609380b2ed1cd763

Abstracted market opening time

FantasyBumps/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 from django.utils.functional import cached_property
 from django.dispatch import receiver
 
-from .constants import genders, money
+from .constants import genders, timings, money
 
 
 class Event(models.Model):
@@ -34,7 +34,7 @@ class Event(models.Model):
         """
         
         now = timezone.now()
-        day_shift = timedelta(1) if now.time() >= time(20, 00) else timedelta(0)
+        day_shift = timedelta(1) if now.time() >= timings.MARKET_OPENS else timedelta(0)
         date = now.date() + day_shift
         
         day = self.days.filter(date__gte = date).first()
@@ -114,7 +114,7 @@ class Day(models.Model):
         
         return datetime.combine(
             self.date - timedelta(1 if earlier_days else 4),
-            time(hour = 20),
+            timings.MARKET_OPENS,
             timezone.now().tzinfo,
         )
     

FantasyBumps/constants.py
@@ -1,8 +1,14 @@
+from datetime import time
+
 
 class genders:
     MENS = 'M'
     WOMENS = 'W'
     TOTALS = 'T'
+
+
+class timings:
+    MARKET_OPENS = time(20, 00)
 
 
 class money:

commit 152b676668265f9aa5b7580c337d0a356dfa0cb0

Fixed linter error introduced by 56a2395

FantasyBumps/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime, time, timedelta
+from datetime import datetime, timedelta
 from functools import lru_cache
 
 from django.db import models