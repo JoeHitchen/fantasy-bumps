JoeHitchen/fantasy-bumps

commit c5dc73db96f5b306943934e2860d188a491e0f98

Crew.results accepts prefetched data

FantasyBumps/models.py
@@ -228,14 +228,24 @@ class Crew(models.Model):
     def results(self, day):
         """The crew's result yesterday and for the week."""
         
-        position_start = self.positions.get(day = day.event.days.first())
-        position_today = self.positions.get(day = day)
+        # Start position
+        if hasattr(self, '_posn_start'):
+            position_start = self._posn_start[0]
+        else:
+            position_start = self.positions.get(day = day.event.days.first())
         
-        position_yest = self.positions.filter(day = day.prev)
-        if not position_yest:
+        # Yesterday's position
+        position_yest_queryset = self._posn_yest if hasattr(self, '_posn_yest') else self.positions.filter(day = day.prev)
+        if not position_yest_queryset:
             return {'week': 0, 'yesterday': 0}
         else:
-            position_yest = position_yest[0]
+            position_yest = position_yest_queryset[0]
+        
+        # Today's position
+        if hasattr(self, '_posn_today'):
+            position_today = self._posn_today[0]
+        else:
+            position_today = self.positions.get(day = day)
         
         return {
             'week': position_start.rank - position_today.rank,

FantasyBumps/models_tests.py
@@ -661,6 +661,22 @@ class Test__Crew(TestCase):
         
         with self.assertNumQueries(5):
             self.crew_top.results(self.day3)
+    
+    
+    @tag('query-count')
+    def test__results__query_count__prefetched(self):
+        """Expect no queries."""
+        
+        self.crew_top._posn_start = list(self.crew_top.positions.filter(day = self.day1))
+        self.crew_top._posn_yest = list(self.crew_top.positions.filter(day = self.day2))
+        self.crew_top._posn_today = list(self.crew_top.positions.filter(day = self.day3))
+        
+        with self.assertNumQueries(0):
+            self.crew_top.results(self.day3)
+        
+        del self.crew_top._posn_start
+        del self.crew_top._posn_yest
+        del self.crew_top._posn_today
 
 
 

commit 42a26dac2f8006c8296d303749250981ddd5c442

Fixed linter issue introduced by c5dc73d

FantasyBumps/models.py
@@ -235,7 +235,10 @@ class Crew(models.Model):
             position_start = self.positions.get(day = day.event.days.first())
         
         # Yesterday's position
-        position_yest_queryset = self._posn_yest if hasattr(self, '_posn_yest') else self.positions.filter(day = day.prev)
+        if hasattr(self, '_posn_yest'):
+            position_yest_queryset = self._posn_yest
+        else:
+            position_yest_queryset = self.positions.filter(day = day.prev)
         if not position_yest_queryset:
             return {'week': 0, 'yesterday': 0}
         else: