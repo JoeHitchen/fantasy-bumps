JoeHitchen/fantasy-bumps

commit 5a57fdbb698b36e8d1a91bf1ed1a68c1a39ee9cb

Protected against missing crew lists

FantasyBumps/management/commands/common.py
@@ -43,12 +43,13 @@ def add_athletes(event, crews, crew_lists):
     athletes = []
     for crew_id, crew in crews.items():
         for seat_id, seat in seats.items():
-            athletes.append(models.Athlete(
-                event = event,
-                crew = crew,
-                seat = seat,
-                name = crew_lists[crew_id][seat_id],
-            ))
+            if 'crew_id' in crew_lists and 'seat_id' in crew_lists[crew_id]:
+                athletes.append(models.Athlete(
+                    event = event,
+                    crew = crew,
+                    seat = seat,
+                    name = crew_lists[crew_id][seat_id],
+                ))
     
     models.Athlete.objects.bulk_create(athletes)
 

commit 2fd01b059e678a68bf2d25cda55f0e61292981ad

Fixed issue introduced by 5a57fdb preventing crew lists from loading

FantasyBumps/management/commands/common.py
@@ -43,7 +43,7 @@ def add_athletes(event, crews, crew_lists):
     athletes = []
     for crew_id, crew in crews.items():
         for seat_id, seat in seats.items():
-            if 'crew_id' in crew_lists and 'seat_id' in crew_lists[crew_id]:
+            if crew_id in crew_lists and seat_id in crew_lists[crew_id]:
                 athletes.append(models.Athlete(
                     event = event,
                     crew = crew,