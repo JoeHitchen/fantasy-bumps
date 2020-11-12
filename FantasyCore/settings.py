69ba90f81c4fd7d9c71a4104667e7ca8262e4a23
Joe Hitchen <hitchenjoe+git@gmail.com>
2020-11-12 10:33:50 +0000

    Renamed DATABASE_PASS variable

--- FantasyCore/settings.py
+++ FantasyCore/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
         'HOST': os.environ.get('DATABASE_HOST', ''),
         'PORT': os.environ.get('DATABASE_PORT', ''),
         'USER': os.environ.get('DATABASE_USER', ''),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'PASSWORD': os.environ.get('DATABASE_PASS'),
         'NAME': os.environ.get('DATABASE_NAME', 'database.sqlite3'),
         'TEST': {
             'NAME': os.environ.get('DATABASE_NAME_TEST', 'test.sqlite3'),
