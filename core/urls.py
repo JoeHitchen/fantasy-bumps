80ec0de2feeba9cedb9dc31fe12f721627d09f32
Joe Hitchen <hitchenjoe+git@gmail.com>
2021-06-06 18:16:21 +0100

    Added crab emoji as favicon

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,5 @@
+import os
+
 from django.urls import path, include
 from django.conf.urls.static import static
 
@@ -15,3 +17,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(
+        'favicon.ico',
+        document_root = os.path.join(settings.BASE_DIR, 'core', 'static', 'favicon.ico'),
+    )
