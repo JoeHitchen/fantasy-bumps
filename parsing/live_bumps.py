2b64064d4a226ca80485ae46d44072c23c4109d9
Joe Hitchen <hitchenjoe+git@gmail.com>
2022-06-20 18:44:44 +0100

    Refactored Live Bumps crew list parsing

--- parsing/live_bumps.py
+++ parsing/live_bumps.py
@@ -1,8 +1,11 @@
 import html
+import logging
 
 import requests
 
-from .common import seat_parser, boat_code_parser, TORPIDS, EIGHTS, MEN, WOMEN
+from .common import TORPIDS, EIGHTS, MEN, WOMEN, series_text_map, seat_parser, boat_code_parser
+
+logger = logging.getLogger(__name__)
 
 
 def _crew_results(crew_data):
@@ -12,6 +15,13 @@ def _crew_results(crew_data):
         positions.append(positions[-1] - move['moves'])  # Sign reversed
     
     return positions
+
+
+def _parse_crew_list(crew_data):
+    return {
+        seat_parser(person['pos']): html.unescape(person['name'])
+        for person in crew_data
+    }
 
 
 def get_positions(series, year, day_number):
@@ -42,35 +52,27 @@ def get_positions(series, year, day_number):
 
 
 def get_crew_lists(series, year):
-    event_string = {TORPIDS: 'Torpids', EIGHTS: 'Eights'}[series]
-    print('Retriving crew lists for {} {} via Live Bumps'.format(event_string, year))  # noqa: T201
+    """Generates a crew/crew-list map from the Live Bumps records."""
     
-    url = 'https://bumps.live/data/{}_{}_crews.json'.format(event_string.lower(), year)
-    response = requests.get(url)
+    series_text = series_text_map[series]
+    logger.info(f'Retriving crew lists for {series_text} {year} from Live Bumps')
     
-    if not response.status_code == 200:
-        raise IOError('Could not load crew lists page')
+    # Load data
+    response = requests.get(f'https://bumps.live/data/{series_text.lower()}_{year}_crews.json')
+    if not response.ok:
+        raise response.raise_for_status()
     
-    crews = {}
+    # Extract crew lists
+    crew_lists = {}
     for boat_code, club_data in response.json().items():
         club = boat_code_parser(boat_code)
         
         for crew_rank, crew_data in club_data['men'].items():
-            
-            crew_list = {
-                seat_parser(person['pos']): html.unescape(person['name'])
-                for person in crew_data
-            }
-            crews[(club, MEN, int(crew_rank))] = crew_list
-        
+            crew_lists[(club, MEN, int(crew_rank))] = _parse_crew_list(crew_data)
         
         for crew_rank, crew_data in club_data['women'].items():
-            
-            crew_list = {
-                seat_parser(person['pos']): html.unescape(person['name'])
-                for person in crew_data
-            }
-            crews[(club, WOMEN, int(crew_rank))] = crew_list
+            crew_lists[(club, WOMEN, int(crew_rank))] = _parse_crew_list(crew_data)
     
-    return crews
+    logger.info(f'Retrieved {len(crew_lists)} crews from Live Bumps for {series_text} {year}')
+    return crew_lists
 
