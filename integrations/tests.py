958940da3370ca405c6503b5f3320afa05ec1ef7
Joe Hitchen <hitchenjoe+git@gmail.com>
2023-03-07 18:06:54 +0000

    Extended the CamFM start order generation to dynamically generate divisions

--- integrations/tests.py
+++ integrations/tests.py
@@ -474,4 +474,22 @@ class Test__CamFM(TestCase):
                 for index, division in enumerate(parsed):
                     with self.subTest('{}Div{}'.format(division['gender'], division['number'])):
                         self.assertEqual(division, expected[index])
+    
+    
+    def test__start_orders__smoke(self) -> None:
+        """Checks that other historical events can be retrieved without error."""
+        
+        events = [
+            (MAYS, 2017, 9, 155),
+            (MAYS, 2018, 9, 154),
+            (MAYS, 2019, 11, 168),
+        ]
+        
+        for series, year, num_divs, num_crews in events:
+            for day_number in range(1, 6):
+                with self.subTest([series, year, day_number]):
+                    
+                    start_order = camfm.get_start_order(series, year, day_number)
+                    self.assertEqual(len(start_order), num_divs)
+                    self.assertEqual(sum(len(div['crews']) for div in start_order), num_crews)
 
