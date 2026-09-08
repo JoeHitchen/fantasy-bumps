4fe3c2b77dc923ed5d5d505a76e52752844ef020
JoeHitchen <hitchenjoe@gmail.com>
2026-09-08 18:36:33 +0100

    Migrates tooling to UV for easier adoption of agentic workflows (#8)

--- project.py
+++ project.py
@@ -5,31 +5,17 @@ if not args:
     raise IndexError('Must supply at least one argument')
 
 commands = {
-    'test': ['pytest', 'fantasy core'],
-    'test:ff': ['pytest -x', 'fantasy core'],
-    'test:external': ['pytest', 'integrations'],
-    'type': ['mypy'],
-    'lint': ['flake8'],
-    'markdown': [
-        'pymarkdownlnt',
-        '--disable-rules=line-length',
-        'scan',
-        '--respect-gitignore',
-        '-r',
-        '.',
-    ],
-    'markdown:fix': [
-        'pymarkdownlnt',
-        '--disable-rules=line-length',
-        'fix',
-        '--respect-gitignore',
-        '-r',
-        '.',
-    ],
+    'test': ('uv run pytest', 'fantasy core'),
+    'test:ff': ('uv run pytest -x', 'fantasy core'),
+    'test:external': ('uv run pytest', 'integrations'),
+    'type': ('uv run mypy', ''),
+    'lint': ('uv run flake8', ''),
+    'markdown': ('uv run pymarkdownlnt scan -r', '.'),
+    'markdown:fix': ('uv run pymarkdownlnt fix -r', '.'),
 }
 
 command_raw = args[0]
-command_parts = commands.get(command_raw, ['python manage.py {}'.format(command_raw)])
+command_parts = commands.get(command_raw, ('python manage.py {}'.format(command_raw), ''))
 command_str = ' '.join([command_parts[0], *(args[1:] if len(args) > 1 else command_parts[1:])])
 status = os.system(command_str)
 
